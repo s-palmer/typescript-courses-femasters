3026ff5f960911931a2fdb6967847d025dd90dd2
Sergei Palmer <10038681+s-palmer@users.noreply.github.com>
2024-06-26 10:05:23 +0100

    configure esling.config.mjs

---
+++ packages/chat-stdlib/eslint.config.mjs
@@ -0,0 +1,33 @@
+import globals from "globals";
+import typescriptPlugin from "@typescript-eslint/eslint-plugin"; // TypeScript ESLint plugin
+import typescriptParser from "@typescript-eslint/parser"; // TypeScript parser
+
+export default [
+  // Configuration for JavaScript and TypeScript files
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], // Include relevant file types
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parser: typescriptParser,
+      parserOptions: {
+        project: './tsconfig.json', // Ensure TypeScript checks are performed according to tsconfig.json
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptPlugin, // Load the TypeScript ESLint plugin
+    },
+    ignores: ["dist/**", "eslint.config.mjs"], // Ignore dist and config files
+    rules: {
+      ...typescriptPlugin.configs.recommended.rules,
+      ...typescriptPlugin.configs['recommended-requiring-type-checking'].rules,
+      
+      // Additional rules
+      "prefer-const": "error", // Prefer const over let where possible
+      "@typescript-eslint/no-unused-vars": "off", // Disable no-unused-vars rule
+      "@typescript-eslint/no-unused-params": "off", // Disable no-unused-params rule
+    }
+  }
+];
