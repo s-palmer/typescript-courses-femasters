02fdb6396985f837c8ce852ae2acf2549232632e
Sergei Palmer <10038681+s-palmer@users.noreply.github.com>
2024-06-26 18:16:23 +0100

    Introduce type guards

--- packages/chat/src/data/teams.ts
+++ packages/chat/src/data/teams.ts
@@ -1,10 +1,16 @@
 import { apiCall } from '../utils/networking'
 import type { ITeam } from '../types'
+import { isTypedArray, isTeam } from '../type-guards'
 
-let cachedAllTeamsList
+let cachedAllTeamsList: Promise<ITeam[]>
+
 export async function getAllTeams(): Promise<ITeam[]> {
   if (typeof cachedAllTeamsList === 'undefined')
-    cachedAllTeamsList = apiCall('teams')
+    cachedAllTeamsList = apiCall('teams').then((responseData) => {
+      if (isTypedArray(responseData, isTeam)) return responseData
+      else
+        throw new Error('getAllTeams: Invalid data returned from API')
+    })
 
   return await cachedAllTeamsList
 }
