9c0147f180799ba15072906140071e520a7daf5e
Sergei Palmer <10038681+s-palmer@users.noreply.github.com>
2024-06-26 10:14:04 +0100

    fix up eslint config

--- packages/chat-stdlib/eslint.config.mjs
+++ packages/chat-stdlib/eslint.config.mjs
@@ -1,11 +1,11 @@
-import globals from "globals";
-import typescriptPlugin from "@typescript-eslint/eslint-plugin"; // TypeScript ESLint plugin
-import typescriptParser from "@typescript-eslint/parser"; // TypeScript parser
+import globals from 'globals'
+import typescriptPlugin from '@typescript-eslint/eslint-plugin' // TypeScript ESLint plugin
+import typescriptParser from '@typescript-eslint/parser' // TypeScript parser
 
 export default [
   // Configuration for JavaScript and TypeScript files
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], // Include relevant file types
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], // Include relevant file types
     languageOptions: {
       globals: {
         ...globals.browser,
@@ -19,15 +19,31 @@ export default [
     plugins: {
       '@typescript-eslint': typescriptPlugin, // Load the TypeScript ESLint plugin
     },
-    ignores: ["dist/**", "eslint.config.mjs"], // Ignore dist and config files
+    ignores: ['dist/**'], // Ignore dist and config files
     rules: {
       ...typescriptPlugin.configs.recommended.rules,
-      ...typescriptPlugin.configs['recommended-requiring-type-checking'].rules,
-      
-      // Additional rules
-      "prefer-const": "error", // Prefer const over let where possible
-      "@typescript-eslint/no-unused-vars": "off", // Disable no-unused-vars rule
-      "@typescript-eslint/no-unused-params": "off", // Disable no-unused-params rule
-    }
-  }
-];
+      ...typescriptPlugin.configs[
+        'recommended-requiring-type-checking'
+      ].rules,
+
+      'prefer-const': 'error', // Prefer const over let where possible
+      '@typescript-eslint/no-unused-vars': 'off', // Disable no-unused-vars rule
+      '@typescript-eslint/no-unused-params': 'off', // Disable no-unused-params rule
+    },
+  },
+  {
+    files: ['tests/**/*.ts'],
+    env: {
+      node: true,
+      jest: true,
+    },
+  },
+  // Override for .eslintrc.js file itself
+  {
+    files: ['.eslintrc.js'],
+    extends: ['plugin:@typescript-eslint/disable-type-checked'],
+    rules: {
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+    },
+  },
+]
